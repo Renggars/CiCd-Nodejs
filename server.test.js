9d5afedac25a5818177fa0e82fa4f600dfe7f155
Renggars <renggarendy22@gmail.com>
2025-08-16 19:13:31 +0700

    fix server.test.js
    Signed-off-by: Renggars <renggarendy22@gmail.com>

--- server.test.js
+++ server.test.js
@@ -2,33 +2,32 @@ const app = require("./server"); // Link to your server file
 const supertest = require("supertest");
 const request = supertest(app);
 
-it("Call the /github endpoint", async (done) => {
+it("Call the /github endpoint", async () => {
   const res = await request.get("/github");
   expect(res.status).toBe(200);
   expect(res.text).toBe("Hello, github indonesia!");
-  done();
 });
-it("Call the /youtube endpoint", async (done) => {
+
+it("Call the /youtube endpoint", async () => {
   const res = await request.get("/youtube");
   expect(res.status).toBe(200);
   expect(res.text).toBe("Hello, youtube indonesia!");
-  done();
 });
-it("Call the / endpoint", async (done) => {
+
+it("Call the / endpoint", async () => {
   const res = await request.get("/");
   expect(res.status).toBe(200);
   expect(res.text).toBe("This App is running properly!");
-  done();
 });
-it("Call the /pong endpoint", async (done) => {
+
+it("Call the /pong endpoint", async () => {
   const res = await request.get("/ping");
   expect(res.status).toBe(200);
   expect(res.text).toBe("Pong!");
-  done();
 });
-it("Call the /hello/:name endpoint", async (done) => {
+
+it("Call the /hello/:name endpoint", async () => {
   const res = await request.get("/hello/Iqbal");
   expect(res.status).toBe(200);
   expect(res.body.message).toBe("Hello Iqbal");
-  done();
 });
